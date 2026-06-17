6e0e71e81e3c524d9669d7aee0a7d1f6e5b675a3
nehapawar-hub <nehapawarpawar3697@gmail.com>
2026-06-17 12:26:33 +0530

    app js added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
-// eslint-disable-next-line no-unused-vars
-import {Home} from './Home';
-// eslint-disable-next-line no-unused-vars
-import {Department} from './Department';
-import {Employee} from './Employee';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-
+import { Home } from './Home';
+import { Department } from './Department';
+import { Employee } from './Employee';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/department" element={<Department />} />
-          <Route path="/employee" element={<Employee />} />
-        </Routes>
-      </BrowserRouter>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App container">
+        <h3 className="d-flex justify-content-center m-3">
+          My React App
+        </h3>
+
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/department">
+                Department
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink className="btn btn-light btn-outline-primary" to="/employee">
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/home' component={Home}></Route>
+          <Route path='/department' component={Department}></Route>
+          <Route path='/employee' component={Employee}></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
